8262faec6180a022a234734ddb8f1c27eea02947
promex04 <laihieu2714@gmail.com>
2026-04-10 19:57:21 +0700

    notch: update to version 1.0.3

--- Casks/notch.rb
+++ Casks/notch.rb
@@ -1,6 +1,6 @@
 cask "notch" do
-  version "1.0.2"
-  sha256 "43141dc0ac35bf0be04dad8db1fc4c66a5463956d343ce6aaf6879688fc30670"
+  version "1.0.3"
+  sha256 "7c55e5dad92514aa84d46686fc2bb1eeffb11584d487e77bf42f1fce85fceedd"
 
   url "https://github.com/ProMeX04/notch-releases/releases/download/v#{version}/Notch-#{version}.zip"
   name "Notch"
